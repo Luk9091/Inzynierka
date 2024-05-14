51a2657402a463756f35fa7ad5cd884f47913c75
Łukasz Przystupa <lukasz9091s@gmail.com>
2024-05-14 11:52:14 +0200

    Servo lib

--- Hardware/pico/Servo/servo.c
+++ Hardware/pico/Servo/servo.c
@@ -1,29 +1,97 @@
 #include "servo.h"
-#include <pico/stdlib.h>
-
-#include "servo.pio"
-
-#define SERVO_PIN_1 2
-#define SERVO_PIN_2 3
-#define SERVO_PIN_3 4
-#define SERVO_PIN_4 5
-
-static PIO ENGINE;
-static uint stateMachine;
+#include <stdio.h>
+#include <hardware/clocks.h>
 
 
-void Servo_Init(){
-    uint pins = 1 << SERVO_PIN_1 | 1 << SERVO_PIN_2 | 1 << SERVO_PIN_3 | 1 << SERVO_PIN_4;
-    gpio_init_mask(pins);
-    gpio_set_dir_out_masked(pins);
+// Servo need 50Hz, f_sys = 125HMz
+#define PRESCALER   100
+#define TOP       24999
 
-    ENGINE = pio0;
-    uint offset = pio_add_program(ENGINE, &servo_program);
-    stateMachine = pio_claim_sm_mask(ENGINE, true);
-    servo_program_init(ENGINE, stateMachine, offset)
+#define LEVEL_MIN 600
+#define LEVEL_MAX 3200
 
+#define ADC_MAX 3850
+#define ADC_MIN 260
+
+
+void Servo_init(Servo_t *servo){
+    // static uint PWM_STATE = 0;
+    static uint ADC_STATE = 0;
+    servo->start = true;
+
+    if (servo->step == 0)
+        servo->step = 1;
+
+    // PWM INIT
+    gpio_set_function(servo->GPIO, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(servo->GPIO);
+
+    pwm_config config = pwm_get_default_config();
+
+    config.div = PRESCALER << PWM_CH0_DIV_INT_LSB;
+    config.top = TOP;
+
+    pwm_init(slice_num, &config, true);
+
+    if (servo->ADC_PIN >= 26 && servo->ADC_PIN <= 28){
+        adc_gpio_init(servo->ADC_PIN);
+        servo->current_angle = Servo_readAngle(servo);
+    } else {
+        servo->current_angle = 0;
+    }
 }
 
-void Servo_step(uint number){
-    pio_sm_put(LEFT_ENGINE,  stateMachine)
+
+void Servo_setAngle(Servo_t *servo, uint angle){
+    if (angle > servo->max){angle = servo->max;} else
+    if (angle < servo->min){angle = servo->min;}
+
+    uint16_t level = LEVEL_MIN + angle * ((LEVEL_MAX - LEVEL_MIN) / 180);
+    servo->current_angle = angle;
+    pwm_set_gpio_level(servo->GPIO, level);
+}
+
+
+int Servo_readAngle(Servo_t *servo){
+    adc_select_input(servo->ADC_PIN - 26);
+    int value = adc_read();
+
+    value = ((value - ADC_MIN) * 180) / (ADC_MAX - ADC_MIN);
+    if (servo->flipRead)
+        value = 180 - value;
+    return value;
+}
+
+
+void Servo_goto(Servo_t *servo){
+    if (!servo->start) return;
+    
+    uint small_step = servo->step;
+
+    if (servo->current_angle != servo->angle){
+        if (abs(servo->angle - servo->current_angle) < servo->step){
+            small_step = abs(servo->angle - servo->current_angle);
+        }
+        if(servo->current_angle > servo->angle){
+            servo->current_angle -= small_step;
+        } else {
+            servo->current_angle += small_step;
+        }
+
+        Servo_setAngle(servo, servo->current_angle);
+    }
+}
+
+void Servo_start(Servo_t *servo){
+    servo->start = true;
+    servo->angle = servo->backUp;
+    Servo_setAngle(servo, servo->angle);
+    uint slice = pwm_gpio_to_slice_num(servo->GPIO);
+    pwm_set_enabled(slice, true);
+}
+
+void Servo_stop(Servo_t *servo){
+    servo->start = false;
+    uint slice = pwm_gpio_to_slice_num(servo->GPIO);
+    pwm_set_enabled(slice, false);
 }
--- Hardware/pico/Servo/servo.h
+++ Hardware/pico/Servo/servo.h
@@ -1,12 +1,51 @@
 #ifndef __SERVO__H
 #define __SERVO__H
 
-#include <stdio.h>
+#include <stdlib.h>
 #include <pico/stdlib.h>
-#include <hardware/gpio.h>
-#include <hardware/pio.h>
 
-void Servo_Init();
-void Servo_step(uint number);
+#include <hardware/gpio.h>
+#include <hardware/pwm.h>
+#include <hardware/adc.h>
+
+
+#define SERVO_ARM_PIN       21
+#define SERVO_BASE_PIN      20
+#define SERVO_ROTOR_PIN     19
+#define SERVO_HAND_PIN      18
+
+/*
+ * Servo config struct
+ * if not use check servo position change only GPIO
+ * On default:
+ * ADC_PIN = 0         -- not use
+ * enableMux = 0       -- not use
+ * enableState = false -- use only if enabled mux is used
+*/
+typedef struct SERVO {
+    bool start;
+    uint GPIO;
+    uint ADC_PIN;
+    bool flipRead;
+    int angle;
+    int current_angle;
+    uint step;
+    int backUp;
+    int min;
+    int max;
+} Servo_t;
+
+// Servo initialization function, enable PWM
+void Servo_init(Servo_t *servo);
+
+
+
+void Servo_setAngle(Servo_t *servo, uint angle);
+int Servo_readAngle(Servo_t *servo);
+
+void Servo_goto(Servo_t *servo);
+
+void Servo_start(Servo_t *servo);
+void Servo_stop(Servo_t *servo);
 
 #endif
