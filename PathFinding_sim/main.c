6e00ac63fe3321d1be13c46ccf6e186b41529d9a
Łukasz Przystupa <lukasz9091s@gmail.com>
2024-06-23 13:57:40 +0200

    Color printf, test main

--- PathFinding_sim/main.c
+++ PathFinding_sim/main.c
@@ -3,6 +3,7 @@
 
 #include <raylib.h>
 
+#include "printf_color.h"
 #include "map.h"
 #include "test_map.h"
 #include "pathfinding.h"
@@ -10,10 +11,21 @@
 
 int main(){
     printf("Hello, world!\n");
+    TEST_MAP_crossMap();
+    point_t path[MAP_SIZE_X * MAP_SIZE_Y] = {0};
+    point_t start = {0, 0};
+    point_t end = {MAP_SIZE_X - 1, MAP_SIZE_Y - 1};
 
-    // PATHFINDING_init(0, 0, 0, 1);
+    int pathStep = PATHFINDING_dijkstra(start, end, NORTH, path);
+
+    if (pathStep == 0){
+        printf(T_RED "No path found\n" T_RESET);
+    } else {
+        for (int i = 0; i < pathStep; i++){
+        printf("Path[%d]: %d, %d\n", i, path[i].x, path[i].y);
+        }
+    }
     
-
     InitWindow(screenWidth, screenHeight, "Map test");
     SetTargetFPS(FPS_LIMIT);
     Color background = {
@@ -22,18 +34,18 @@ int main(){
         .b = 0x30
     };
 
-    
-    TEST_MAP_crossMap();
-
     while (!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(DARKGRAY);
-        DrawText("Working in progress", 0, 0, 24, LIGHTGRAY);
+        // DrawText("Working in progress", 0, 0, 24, LIGHTGRAY);
 
-        MAP_draw();
-        MAP_drawGrid();
-        CAR_draw(0, 0);
-        CAR_draw(0, 1);
+        MAP_draw(MAP_OFFSET_X, MAP_OFFSET_Y);
+        MAP_drawGrid(MAP_OFFSET_X, MAP_OFFSET_Y);
+        
+        
+        for (int i = 0; i < pathStep; i++){
+            CAR_draw(path[i].x, path[i].y, MAP_OFFSET_X, MAP_OFFSET_Y);
+        }
         
         EndDrawing();
     }
