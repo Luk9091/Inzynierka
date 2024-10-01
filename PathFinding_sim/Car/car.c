462ba27471650fa585f5c6e97e492558b274c88d
Łukasz Przystupa <lukasz9091s@gmail.com>
2024-10-01 15:32:45 +0200

    Car better turn, add backwardMove

--- PathFinding_sim/Car/car.c
+++ PathFinding_sim/Car/car.c
@@ -1,15 +1,18 @@
 #include "car.h"
+#include <time.h>
 
 static car_t car;
 static point_t path[MAP_SIZE_X * MAP_SIZE_Y * CELL_SIZE_X * CELL_SIZE_Y] = {0};
+
 static uint step;
 static int pathLen;
-
+static float beamDistance[3] = {0};
+static double path_time = 0;
 
 void _CAR_moveTriangle(){
     Vector2 center = {
-        .x  = car.position.x * CELL_SIZE_X - cosf(car.angle) * CELL_SIZE_X * CELL_SIZE_X / 8 +MAP_OFFSET_X,
-        .y  = car.position.y * CELL_SIZE_Y - sinf(car.angle) * CELL_SIZE_Y * CELL_SIZE_Y / 8 +MAP_OFFSET_Y
+        .x  = car.position.x * PIXEL_SIZE_X - cosf(car.angle) * CELL_SIZE_X * PIXEL_SIZE_X / 8 +MAP_OFFSET_X,
+        .y  = car.position.y * PIXEL_SIZE_Y - sinf(car.angle) * CELL_SIZE_Y * PIXEL_SIZE_Y / 8 +MAP_OFFSET_Y
     };
 
     car.top.x = center.x + cosf(car.angle) * CAR_SIZE_X;
@@ -23,18 +26,44 @@ void _CAR_moveTriangle(){
 
 }
 
+//! Dont work
+// Circle shouldn't move when car is continuos 
+void CAR_drawTurnCircle(){
+    if (getAngle() == 90) return;
+    float angle = (float)(getAngle()-90) * DEG2RAD;
+    point_t center;
+    float r = CAR_PHYSICAL_LEN / sin(angle);
+
+    if (getAngle() > 90){
+        center.x = car.top.x + r * sin(angle);
+        center.y = car.top.y - r * cos(angle);
+    } else {
+        center.x = car.top.x - r * sin(angle);
+        center.y = car.top.y + r * cos(angle);
+    }
+
+    DrawCircleLines(center.x, center.y, r, WHITE);
+}
+
 
 void _CAR_drawMessage(const char *str){
     const static int fontSize = 20;
     static int height = fontSize/2;
-    int width = screenWidth - MAP_SIZE_X*CELL_SIZE_X * 3;
+    // int width = screenWidth - MAP_SIZE_X*CELL_SIZE_X * 7/PIXEL_SIZE_X;
+    int width = screenWidth * 8/10;
 
     DrawText(str, width, height, fontSize, WHITE);
 }
 
+// void _CAR_drawMessage(const char *str, int line){
+//     const static int fontSize = 20;
+//     int height = line * fontSize + (line + 1) * (fontSize/4);
+//     int width = screenHeight * 8/10;
+// }
+
 void _CAR_drawPath(){
     for (int i = step; i < pathLen; i += 1){
-        POINT_draw(path[i].x, path[i].y, ORANGE);
+        POINT_draw(path[i].x, path[i].y, PIXEL_SIZE_X/4, ORANGE);
     }
 }
 
@@ -47,6 +76,17 @@ void CAR_init(float x, float y, float direction){
     CAR_setAngle(direction);
 }
 
+void CAR_setBeamDistance(float left, float center, float right){
+    const static float mux = (float)(PIXEL_SIZE_X) / (float)(DISTANCE_PER_PIXEL);
+    beamDistance[0] = left   * mux;
+    beamDistance[1] = center * mux;
+    beamDistance[2] = right  * mux;
+
+    for (int i = 0; i < 3; i++){
+        if (beamDistance[i] < 0)
+            beamDistance[i] = 0;
+    }
+}
 
 Vector2 CAR_measureDistance(float angle){
     angle = (angle - 90) * DEG2RAD + car.angle;
@@ -69,9 +109,14 @@ void CAR_draw(){
         car.left, car.top, car.right,
         RED
     );
+    CAR_drawBeam(-30, beamDistance[0]);
+    CAR_drawBeam(  0, beamDistance[1]);
+    CAR_drawBeam( 30, beamDistance[2]);
+
+    // CAR_drawTurnCircle();
 
     
-    const char *str = TextFormat("Car pos: %2.2f, %2.2f\nAngle: %2.2f\nDestination: %2i, %2i", car.position.x, car.position.y, car.angle * RAD2DEG, path[pathLen-1].x, path[pathLen-1].y);
+    const char *str = TextFormat("Car pos: %2.2f, %2.2f\nAngle: %2.2f\nDestination: %2i, %2i\nPathfinding time: %.4f", car.position.x, car.position.y, car.angle * RAD2DEG, path[pathLen-1].x, path[pathLen-1].y, path_time);
     _CAR_drawMessage(str);
 }
 
@@ -87,7 +132,7 @@ void CAR_drawBeam(float angle, float radius){
 
 void CAR_setAngle(float angle){
     // angle = angle - 90;
-    if (angle > 360){
+    if (angle >= 360){
         angle -= 360;
     } else if (angle < 0){
         angle += 360;
@@ -96,7 +141,12 @@ void CAR_setAngle(float angle){
     _CAR_moveTriangle();
 }
 
+float CAR_getAngle(){
+    return car.angle;
+}
+
 void CAR_changeAngle(float angle){
+    CAR_setBeamDistance(0, 0, 0);
     CAR_setAngle((car.angle * RAD2DEG) + angle);
 }
 
@@ -106,29 +156,70 @@ void CAR_setPosition(int x, int y){
     _CAR_moveTriangle();
 }
 
+Vector2 CAR_getPosition(){
+    return car.position;
+}
+
 bool CAR_move(){
+    if (!calculateSpeed()) return false;
     bool collision = false;
+    float deltaAngle = abs(getAngle() - 90) * DEG2RAD;
+    
     Vector2 move = {
-        .x = rounds(cosf(car.angle), 2),
-        .y = rounds(sinf(car.angle), 2)
+        .x = rounds(cos(deltaAngle)*cosf(car.angle), 2),
+        .y = rounds(cos(deltaAngle)*sinf(car.angle), 2)
     };
     Vector2 newPos = {
         .x = car.position.x + move.x,
         .y = car.position.y + move.y
     };
 
-    if (MAP_collisionDetect(newPos.x, car.position.y)){
+    if (MAP_collisionDetect(newPos.x, car.position.y) || !(car.position.x + move.x >= 0 && car.position.x + move.x < (MAP_SIZE_X*CELL_SIZE_X))){
         newPos.x = roundf(car.position.x) - move.x/100;
         collision = true;
     } 
-    if (MAP_collisionDetect(car.position.x, newPos.y)){
+    if (MAP_collisionDetect(car.position.x, newPos.y) || !(car.position.y + move.y >= 0 && car.position.y + move.y < (MAP_SIZE_Y*CELL_SIZE_Y))){
         newPos.y = roundf(car.position.y) - move.y/100;
         collision = true;
     } 
 
 
     car.position = newPos;
+    CAR_changeAngle((90 - getAngle())/2);
     
+    CAR_setBeamDistance(0, 0, 0);
+    _CAR_moveTriangle();
+    return !collision;
+}
+
+bool CAR_moveBackward(){
+    if (!calculateSpeed()) return false;
+    bool collision = false;
+    float deltaAngle = abs(getAngle() - 90) * DEG2RAD;
+    
+    Vector2 move = {
+        .x = - rounds(cos(deltaAngle)*cos(car.angle), 2),
+        .y = - rounds(cos(deltaAngle)*sin(car.angle), 2)
+    };
+    Vector2 newPos = {
+        .x = car.position.x + move.x,
+        .y = car.position.y + move.y
+    };
+
+    if (MAP_collisionDetect(newPos.x, car.position.y) || !(car.position.x + move.x >= 0 && car.position.x + move.x < (MAP_SIZE_X*CELL_SIZE_X))){
+        newPos.x = roundf(car.position.x) - move.x/100;
+        collision = true;
+    } 
+    if (MAP_collisionDetect(car.position.x, newPos.y) || !(car.position.y + move.y >= 0 && car.position.y + move.y < (MAP_SIZE_Y*CELL_SIZE_Y))){
+        newPos.y = roundf(car.position.y) - move.y/100;
+        collision = true;
+    } 
+
+
+    car.position = newPos;
+    CAR_changeAngle((90 - getAngle())/2);
+
+    CAR_setBeamDistance(0, 0, 0);
     _CAR_moveTriangle();
     return !collision;
 }
@@ -140,6 +231,7 @@ uint CAR_moveByPath(){
         int y = (int)(path[step].y) - (int)(car.position.y);
         float newAngle = rounds(atan2f(y, x), 2);
         
+        static float oldDelta = 0;
         float delta = newAngle - car.angle;
 
         if (delta > π) {delta -= 2*π;}
@@ -149,18 +241,25 @@ uint CAR_moveByPath(){
         if (delta > maxAngle) delta = maxAngle;
         if (delta < -maxAngle) delta = -maxAngle;
 
-        printf("Delta: %2.2f\n", delta * RAD2DEG);
+        // printf("Delta: %2.2f\n", delta * RAD2DEG);
         car.angle += delta;
-
-        if (!CAR_move()) {
-            car.angle += delta;
-            CAR_move();
-            CAR_findPath(path[pathLen-1].x, path[pathLen-1].y);
+        if ((oldDelta - delta)*RAD2DEG > 10){
+            setDeltaAngle(delta*RAD2DEG);
+        } else {
+            setAngle(90);
         }
+        oldDelta = delta;
+
+        CAR_move();
+        // if (!CAR_move()) {
+        //     // car.angle += delta;
+        //     CAR_move();
+        //     CAR_findPath(path[pathLen-1].x, path[pathLen-1].y);
+        // }
         step++;
+        return true;
     }
-    
-    return step;
+    return false;
 }
 
 int CAR_findPath(uint x, uint y){
@@ -168,7 +267,10 @@ int CAR_findPath(uint x, uint y){
     point_t end = {.x = x, .y = y};
     point_t prefer = {.x = ceilf(cosf(car.angle)), y = ceilf(sinf(car.angle))};
 
+    clock_t t_start = clock();
     pathLen = PATHFINDING_dijkstra(start, end, prefer, path);
+    clock_t t_end = clock();
+    path_time = (double)(t_end - t_start) / CLOCKS_PER_SEC;
 
     step = 1;
     return pathLen;
--- PathFinding_sim/Car/car.h
+++ PathFinding_sim/Car/car.h
@@ -6,13 +6,18 @@
 #include <raylib.h>
 #include <math.h>
 
+#include "GUI.h"
 #include "utility.h"
 #include "pathfinding.h"
 #include "map.h"
+#include "udp.h"
 
 #define CAR_SIZE_X 16/2
 #define CAR_SIZE_Y 16/2
 #define CAR_SEE_RANGE 6
+
+// Odległość przedniej osi od tylnej w mm
+static const float CAR_PHYSICAL_LEN = 135 / PIXEL_SIZE_X;
 
 
 
@@ -32,16 +37,20 @@ typedef struct{
 void CAR_init(float x, float y, float direction);
 void CAR_draw();
 void CAR_drawBeam(float angle, float radius);
+void CAR_setBeamDistance(float left, float center, float right);
 Vector2 CAR_measureDistance(float angle);
 
 
 
 void CAR_setAngle(float angle);
+float CAR_getAngle();
 void CAR_changeAngle(float angle);
 void CAR_setPosition(int x, int y);
+Vector2 CAR_getPosition();
 
 
 bool CAR_move();
+bool CAR_moveBackward();
 uint CAR_moveByPath();
 int CAR_findPath(uint x, uint y);
 
