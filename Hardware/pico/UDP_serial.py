30bd74ab00e497261faa911512f6160d3d5f2965
Łukasz Przystupa <lukasz9091s@gmail.com>
2024-10-01 18:39:47 +0200

    UDP serial select lockalhost

--- Hardware/pico/UDP_serial.py
+++ Hardware/pico/UDP_serial.py
@@ -10,40 +10,77 @@ UDP_SERVER_PORT = 4444
 UDP_CLIENT_PORT = 4445
 run = True
 MAX_UDP_PACKET_SIZE = 128
+ROUTER_ADDRESS       = "12.12.0"
+DEFAULT_PC_ADDRESS   = "12.12.0.150"
+DEFAULT_PICO_ADDRESS = "148"
+
+END_LINE = ""
+STORE_DATA = True
+
+def readArgs() -> None:
+    args = sys.argv[1:]
+    if "--addNewLine" in args:
+        global END_LINE
+        END_LINE="\n"
+        args.remove("--addNewLine")
+
+    if "--noSave" in args:
+        global STORE_DATA
+        STORE_DATA = False
+        args.remove("--noSave")
+
+
+    PC_IP = DEFAULT_PC_ADDRESS
+    if len(args) > 0:
+        if args[0] == "localhost":
+            address = "127.0.0.1"
+            PC_IP = address
+        else:
+            address = f"{ROUTER_ADDRESS}.{args[0]}"
+    else:
+        address = f"{ROUTER_ADDRESS}.{DEFAULT_PICO_ADDRESS}"
+    
+    if len(args) > 1:
+        global UDP_SERVER_PORT
+        UDP_SERVER_PORT = int(args[1])
+
+    pc_address   = (PC_IP, UDP_SERVER_PORT)
+
+    return pc_address, address
+
+
+
+def storeLine(data) -> None:
+    try: 
+        with open("data.csv", "a") as file:
+            file.write(data)
+    except IndexError:
+        pass
+
 
 
 def receive(udp_socket: socket.socket):
-    global run
     while run:
         data, addr = udp_socket.recvfrom(MAX_UDP_PACKET_SIZE)
         data = data.strip(b'\x00')
         data = data.decode("utf-8")
 
-        try: 
-            with open("data.csv", "a") as file:
-                file.write(data)
-        except IndexError:
-            pass
+        if STORE_DATA:
+            storeLine(data)
 
         if data.endswith("\n"):
             data = data + "\r"
-        print(data, end="")
+            end_line = ""
+        else:
+            end_line = END_LINE
+        print(data, end=end_line)
 
 
 
 def main():
-    args = sys.argv[1:]
     global run
-
-    if len(args) > 0:
-        address = f"10.42.0.{args[0]}"
-    else:
-        address = "10.42.0.22"
-    
-    if len(args) > 1:
-        global UDP_SERVER_PORT
-        UDP_SERVER_PORT = int(args[1])
-    pc_address   = ("10.42.0.1", UDP_SERVER_PORT)
+    global END_LINE
+    pc_address, address = readArgs()
 
         
 
