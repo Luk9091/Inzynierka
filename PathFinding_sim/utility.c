c5840f0a083c4fefc5268488c227ac439f612679
Łukasz Przystupa <lukasz9091s@gmail.com>
2024-10-08 13:23:54 +0200

    Sleep

--- PathFinding_sim/utility.c
+++ PathFinding_sim/utility.c
@@ -1,7 +1,52 @@
 #include "utility.h"
+#include <stdio.h>
 #include <math.h>
 
 
 float rounds(float value, uint decimalPlace){
     return (int)(value * powf(10, decimalPlace)) / powf(10, decimalPlace);
+}
+
+int atoli(char *str, int *numbers){
+    int find = 0;
+    bool new = false;
+    int sign = 1;
+    int number = 0;
+
+    for (int i = 0; i < strlen(str); i++){
+        if (str[i] >= '0' && str[i] <= '9'){
+            if (new == false){
+                new = true;
+                if (i != 0 && str[i-1] == '-'){
+                    sign = -1;
+                } else {
+                    sign = 1;
+                }
+            }
+
+            number = number * 10;
+            number = number + (str[i] - '0');
+        } else if (new) {
+            numbers[find] = number * sign;
+            find++;
+            number = 0;
+            new = false;
+        }
+    }
+
+    if (new){
+        numbers[find] = number * sign;
+        find++;
+    }
+
+    return find;
+}
+
+inline int sleep_ms(uint miliseconds){
+    return usleep(miliseconds * 1000);
+}
+
+
+float mapValue(float value, float fromMin, float fromMax, float toMin, float toMax){
+    return (value - fromMin) * (toMax - toMin) / (fromMax - fromMin) + toMin;
 }
--- PathFinding_sim/utility.h
+++ PathFinding_sim/utility.h
@@ -5,21 +5,40 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
+#include <unistd.h>
+
+#include "printf_color.h"
 
 typedef unsigned int uint;
 
 #define π 3.14159f
 
-#define screenWidth 800
-#define screenHeight 600
+#define screenWidth 1280
+#define screenHeight 1010
 #define FPS_LIMIT 60
 
-#define MAP_OFFSET_X 0
+
+#define MAP_SIZE_X 3
+#define MAP_SIZE_Y 3
+
+#define CELL_SIZE_X 8
+#define CELL_SIZE_Y 8
+
+#define PIXEL_SIZE_X 32
+#define PIXEL_SIZE_Y 32
+
+#define MAP_OFFSET_X 2
 #define MAP_OFFSET_Y 0
+
+// Distance in mm
+#define DISTANCE_PER_PIXEL 20
 
 #define DEVICE "PC"
 
 
 float rounds(float value, uint decimalPlace);
+int atoli(char *str, int *numbers);
 
+extern int sleep_ms(uint miliseconds);
+float mapValue(float value, float fromMin, float fromMax, float toMin, float toMax);
 #endif
