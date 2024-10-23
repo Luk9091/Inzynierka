a88334c56a60ab0b569a21dbf224fb69fe96c5a0
Łukasz Przystupa <lukasz9091s@gmail.com>
2024-10-23 19:14:43 +0200

    Turn on and off compass calibration code

--- Hardware/pico/Lib/Compass/compass.c
+++ Hardware/pico/Lib/Compass/compass.c
@@ -5,31 +5,51 @@
 #define COMPASS_CALIBRATION_TIME 10e6
 
 static axis_t offset = {-3907, 2370, 668};
+// static axis_t offset = {-3057, -10677, -4979};
+
+#if COMPASS_AUTO_CALIBRATION_PROCESS
 static axis_t min = {32767, 32767, 32767};
 static axis_t max = {-32768,-32768,-32768};
+#endif
 
 void COMPASS_Init(){
+    gpio_init(COMPASS_INT_GPIO);
+    gpio_set_dir(COMPASS_INT_GPIO, GPIO_IN);
+
     I2C_writeReg(I2C_COMPASS_ADDRESS, COMPASS_CONTROL_1_REG, CONTINUE_MODE | FREQ_10HZ | RANGE_2G | SAMPLE_512);
     I2C_writeReg(I2C_COMPASS_ADDRESS, COMPASS_PERIOD_REG, 0x01);
-
 }
 
 
-void COMPASS_rawRead(axis_t *axis){
+int COMPASS_rawRead(axis_t *axis){
     uint8_t data[6];
-    I2C_readNReg(I2C_COMPASS_ADDRESS, COMPASS_OUT_X_LSB_REG, data, 6);
+    int status = I2C_readNReg(I2C_COMPASS_ADDRESS, COMPASS_OUT_X_LSB_REG, data, 6);
+    if (status < 0) return status;
+
     axis->x = ((int16_t)(data[1] << 8) | data[0]);
     axis->y = ((int16_t)(data[3] << 8) | data[2]);
     axis->z = ((int16_t)(data[5] << 8) | data[4]);
+    return 0;
 }
 
-void COMPASS_read(axis_t *axis){
+int COMPASS_read(axis_t *axis){
+    if (!gpio_get(COMPASS_INT_GPIO)) return 1;
+
+    
     axis_t raw;
-    COMPASS_rawRead(&raw);
+    static axis_t old = {0, 0, 0};
+    int status = COMPASS_rawRead(&raw);
+
+    if (status < 0) return status;
+
 #if COMPASS_AUTO_CALIBRATION_PROCESS == true
     COMPASS_addToCalibrate(&raw);
 #endif
-    *axis = COMPASS_calibrateRaw(raw);
+    const float ALPHA = 0.1f;
+    old = LPF_ai(ALPHA, old, COMPASS_calibrateRaw(raw));
+    *axis = old;
+
+    return 0;
 }
 
 axis_t COMPASS_calibrateRaw(axis_t raw){
@@ -37,20 +57,21 @@ axis_t COMPASS_calibrateRaw(axis_t raw){
     calibrated.x = (raw.x - offset.x);
     calibrated.y = (raw.y - offset.y);
     calibrated.z = (raw.z - offset.z);
+
     return calibrated;
 }
 
 
 float COMPASS_getAzimuth(float x, float y){
-    float angle = atan2f(y, x);
-    if (angle < 0) angle += 2 * π;
-    else if (angle > 2 * π) angle -= 2 * π;
+    float angle = atan2f(y, x) * RAD2DEG + MAGNETIC_DECLINATION;
+    if (angle < -0) angle += 360.f;
+    else if (angle > 360) angle -= 360.f;
 
-    angle = angle * 180 / π + MAGNETIC_DECLINATION;
     return angle;
 }
 
 
+#if COMPASS_AUTO_CALIBRATION_PROCESS
 void COMPASS_addToCalibrate(axis_t *raw){
     if (raw->x < min.x) min.x = raw->x;
     if (raw->x > max.x) max.x = raw->x;
@@ -62,10 +83,11 @@ void COMPASS_addToCalibrate(axis_t *raw){
     if (raw->z > max.z) max.z = raw->z;
 }
 
-void COMPASS_applyCalibration(){
+axis_t COMPASS_applyCalibration(){
     offset.x = (max.x + min.x) / 2;
     offset.y = (max.y + min.y) / 2;
     offset.z = (max.z + min.z) / 2;
+    return offset;
 }
 
 
@@ -76,3 +98,4 @@ axis_t COMPASS_getMin(){
 axis_t COMPASS_getMax(){
     return max;
 }
+#endif
--- Hardware/pico/Lib/Compass/compass.h
+++ Hardware/pico/Lib/Compass/compass.h
@@ -3,14 +3,17 @@
 
 #include <pico/stdio.h>
 #include <pico/stdlib.h>
+#include <hardware/gpio.h>
 
 #include "i2c.h"
 #include "utilites.h"
+#include "my_gpio.h"
 
 #define I2C_COMPASS_ADDRESS 0x0D
+#define COMPASS_INT_GPIO     GPIO_COMPASS_INT_PIN
 #define LUBLIN_MAGNETIC_DECLINATION 7.7f
 #define KRAKOW_MAGNETIC_DECLINATION 6.2f
-#define MAGNETIC_DECLINATION LUBLIN_MAGNETIC_DECLINATION
+#define MAGNETIC_DECLINATION KRAKOW_MAGNETIC_DECLINATION
 
 /*!
  * \brief Select if compass should auto add data to calibrate process
@@ -22,16 +25,18 @@
 #define COMPASS_AUTO_CALIBRATION_PROCESS false
 
 void COMPASS_Init();
-void COMPASS_rawRead(axis_t *axis);
-void COMPASS_read(axis_t *axis);
+int COMPASS_rawRead(axis_t *axis);
+int COMPASS_read(axis_t *axis);
 float COMPASS_getAzimuth(float x, float y);
 
 axis_t COMPASS_calibrateRaw(axis_t raw);
 
+#if COMPASS_AUTO_CALIBRATION_PROCESS
 void COMPASS_addToCalibrate(axis_t *raw);
-void COMPASS_applyCalibration();
+axis_t COMPASS_applyCalibration();
 
 axis_t COMPASS_getMin();
 axis_t COMPASS_getMax();
+#endif
 
 #endif
